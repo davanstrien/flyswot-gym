f95bd1b7452a6aa13d192afb7a16f1ffdf241d91
Daniel van Strien <davanstrien@gmail.com>
2022-06-14 09:59:45 +0100

    clean nbs

--- flyswot_gym/_nbdev.py
+++ flyswot_gym/_nbdev.py
@@ -10,12 +10,13 @@ index = {"return_base_path_deduplicated": "00_core.ipynb",
          "train_valid_split_w_stratify": "00_core.ipynb",
          "prepare_dataset": "00_core.ipynb",
          "prepare_transforms": "00_core.ipynb",
-         "flysotData": "00_core.ipynb",
+         "FlyswotData": "00_core.ipynb",
          "prep_data": "00_core.ipynb",
          "collate_fn": "00_core.ipynb",
          "train_model": "00_core.ipynb",
          "plot_confusion_matrix": "00_core.ipynb",
-         "create_classification_report": "00_core.ipynb"}
+         "create_classification_report": "00_core.ipynb",
+         "create_misclassified_report": "00_core.ipynb"}
 
 modules = ["core.py"]
 
@@ -23,5 +24,4 @@ doc_url = "https://davanstrien.github.io/flyswot_gym/"
 
 git_url = "https://github.com/davanstrien/flyswot_gym/tree/master/"
 
-def custom_doc_links(name):
-    return None
+def custom_doc_links(name): return None
